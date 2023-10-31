04e1664f1a133919fdde57bc6be5bd2d8a43e710
Samantha Flores <740376@student.dusd.net>
2023-10-31 15:24:59 -0700

    Adding tile maps

--- Ruby'sAdventureFloresP7/Assets/Scripts/RubyController.cs
+++ Ruby'sAdventureFloresP7/Assets/Scripts/RubyController.cs
@@ -7,14 +7,20 @@ public class RubyController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+       
     }
 
     // Update is called once per frame
     void Update()
     {
+        float horrizontal = Input.GetAxis("Horizontal");
+        float vertiical = Input.GetAxis("Vertical");
+
+
         Vector2 position = transform.position;
-        position.x = position.x + 0.1f;
+        position.x = position.x + 3.0f * horrizontal * Time.deltaTime;
+        position.y = position.y + 3.0f * vertiical * Time.deltaTime;
+
         transform.position = position;
     }
 }
