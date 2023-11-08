7a0a7ca0983ea6ef0df170cd4e6a454354bfa81b
Samantha Flores <740376@student.dusd.net>
2023-11-08 15:27:00 -0800

    Collectables

--- Ruby'sAdventureFloresP7/Assets/Scripts/RubyController.cs
+++ Ruby'sAdventureFloresP7/Assets/Scripts/RubyController.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class RubyController : MonoBehaviour
 {
+    public float speed = 3.0f;
     public int maxHealth = 5;
     int currentHealth;
 
@@ -29,10 +30,14 @@ public class RubyController : MonoBehaviour
     void FixedUpdate()
     {
         Vector2 position = rigidbody2d.position;
-        position.x = position.x + 3.0f * horizontal * Time.deltaTime;
-        position.y = position.y + 3.0f * vertical * Time.deltaTime;
+        position.x = position.x + speed * horizontal * Time.deltaTime;
+        position.y = position.y + speed * vertical * Time.deltaTime;
 
         rigidbody2d.MovePosition(position);
     }
-
+    void ChangeHealth(int amount)
+    {
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+        Debug.Log(currentHealth + "/" + maxHealth);
+    }
 }
