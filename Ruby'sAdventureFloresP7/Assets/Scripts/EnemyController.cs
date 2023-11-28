debc34907e5c69ee09f52c7116655dc9bc2af09f
Samantha Flores <740376@student.dusd.net>
2023-11-28 15:27:30 -0800

    Sprite Animation
    adding animations to Robot and Ruby

--- Ruby'sAdventureFloresP7/Assets/Scripts/EnemyController.cs
+++ Ruby'sAdventureFloresP7/Assets/Scripts/EnemyController.cs
@@ -13,11 +13,14 @@ public class EnemyController : MonoBehaviour
     float timer;
     int direction = 1;
 
+    Animator animator;
+
     // Start is called before the first frame update
     void Start()
     {
        rigidbody2d = GetComponent<Rigidbody2D>();
         timer = changeTime;
+        animator = GetComponent<Animator>();
 
     }
 
@@ -36,10 +39,14 @@ public class EnemyController : MonoBehaviour
         Vector2 position = rigidbody2d.position;
         if(vertical)
         {
+            animator.SetFloat("Move X", 0);
+            animator.SetFloat("Move Y", direction);
             position.y = position.y + Time.deltaTime * speed * direction;
         }
         else
         {
+            animator.SetFloat("Move X", direction);
+            animator.SetFloat("Move Y", 0);
             position.x = position.x + Time.deltaTime * speed * direction;
         }
        
@@ -49,6 +56,10 @@ public class EnemyController : MonoBehaviour
     void OnCollisionEnter2D(Collision2D other)
     {
       RubyController player = other.gameObject.GetComponent<RubyController>();
+        if (player != null)
+        {
+            player.ChangeHealth(-1);
+        }
     }
 }
 
--- Ruby'sAdventureFloresP7/Assets/Scripts/RubyController.cs
+++ Ruby'sAdventureFloresP7/Assets/Scripts/RubyController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class RubyController : MonoBehaviour
@@ -18,13 +19,15 @@ public class RubyController : MonoBehaviour
     float horizontal;
     float vertical;
 
+    Animator animator;
+    Vector2 lookDirection = new Vector2(1, 0);
+
     // Start is called before the first frame update
     void Start()
     {
        rigidbody2d = GetComponent<Rigidbody2D>();
        currentHealth = maxHealth;
-       
-
+        animator = GetComponent<Animator>();
 
     }
 
@@ -33,6 +36,17 @@ public class RubyController : MonoBehaviour
     {
         horizontal = Input.GetAxis("Horizontal");
         vertical = Input.GetAxis("Vertical");
+
+        Vector2 move = new Vector2(horizontal, vertical);
+
+        if(!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y, 0.0f))
+        {
+            lookDirection.Set(move.x, move.y);
+            lookDirection.Normalize();
+        }
+        animator.SetFloat("Look X", lookDirection.x);
+        animator.SetFloat("Look Y", lookDirection.y);
+        animator.SetFloat("Speed", move.magnitude);
 
         if (isInvincible)
         {
